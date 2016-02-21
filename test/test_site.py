610191f80d7a6170c66b37c805acd52cc39c328e
James Carr <jc3212@ic.ac.uk>
2016-02-21 12:41:14 +0000

    Added tests and removed .pyc files

--- test/test_site.py
+++ test/test_site.py
@@ -1,2 +1,22 @@
 import os
 import benchin
+import unittest
+from flask.ext.api import status
+
+class BenChinTestCase(unittest.TestCase):
+  def setUp(self):
+    self.app = benchin.app.test_client()
+
+
+  def test_hello_world(self):
+    resp = self.app.get('/')
+    self.assertTrue("Hello, world!" in resp.data)
+
+
+  def test_404(self):
+    resp = self.app.get('/404')
+    self.assertTrue(status.is_client_error(resp.status_code))
+
+
+if __name__ == "__main__":
+  unittest.main()
